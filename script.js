7c44b1a1687a2109082ca050b13ae5209e76f28a
rooheed <rooheed.gulistani@gmail.com>
2019-02-02 17:12:04 +0100

    First

---
+++ script.js
@@ -0,0 +1,19 @@
+
+ var username = localStorage.getItem("username");
+ if(!username) {
+
+	username = prompt("Please enter your name...", "Write your name...");
+	
+	localStorage.setItem("username", username);
+	
+	document.getElementById("welcomeText").innerHTML = "Hey" + " " + username + "! " + "Welcome to my page...";
+
+ }
+ else if (username) {
+	 document.getElementById("welcomeText").innerHTML = "Hey" + " " + username + " and " + "Welcome back!";
+ }
+
+ setTimeout(function(){
+	document.getElementById("welcomeText").style.display = "none"; 
+   }, 3000);
+
